562a7b09eb699e10a66bd3aeab93c0d7ce2cc552
hadber <adber@tuta.io>
2026-02-28 16:39:39 +0100

    ai epic fail

--- quant1.py
+++ quant1.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 
 # Load a standard YOLO26 model
-model = YOLO("yolo26n.pt")
+model = YOLO("yolo11n.pt")
 
 # Export to TFLite format with INT8 quantization
 # The 'int8' argument triggers Post-Training Quantization
 # 'data' provides the calibration dataset needed for mapping values
-model.export(format="tflite", int8=True, data="coco8.yaml")
+
+#model.export(format="tflite", int8=True, data="coco8.yaml")
+
+model.export(format="onnx", int8=True, data="coco8.yaml")
