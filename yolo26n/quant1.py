5df2a3269e126a10bc1bef0fd4288f991a618288
harizibriseida-web <harizibriseida@gmail.com>
2026-03-17 16:17:10 +0100

    Add INT8 TFLite model

--- yolo26n/quant1.py
+++ yolo26n/quant1.py
@@ -7,6 +7,6 @@ model = YOLO("yolo11n.pt")
 # The 'int8' argument triggers Post-Training Quantization
 # 'data' provides the calibration dataset needed for mapping values
 
-#model.export(format="tflite", int8=True, data="coco8.yaml")
+model.export(format="tflite", int8=True, data="coco8.yaml")
 
-model.export(format="onnx", int8=True, data="coco8.yaml")
+#model.export(format="onnx", int8=True, data="coco8.yaml")
