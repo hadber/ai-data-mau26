b10113a31e054dc5867de651b93e0de9246d7d06
Smilkos Athanasios <116645341+thanossmilkos@users.noreply.github.com>
2026-03-17 12:26:55 +0100

    Refactor counting script for better readability

--- data/counting/CrowdHuman/count.py
+++ data/counting/CrowdHuman/count.py
@@ -1,23 +1,19 @@
-# Install libraries if needed
-# pip install ultralytics pandas matplotlib
 
 import os
 import pandas as pd
 from ultralytics import YOLO
 
-
-
 from google.colab import drive
 drive.mount('/content/drive')
 
-# -------- SETTINGS --------
+
 image_folder = "/content/drive/MyDrive/Colab Notebooks/AI&DataProject/Images"   # change this to your folder
 model = YOLO("yolo26n.pt")
-# --------------------------
+
 
 people_distribution = {}
 
-# Loop through images
+
 for filename in os.listdir(image_folder):
 
     if filename.lower().endswith((".jpg", ".jpeg", ".png")):
@@ -35,20 +31,17 @@ for filename in os.listdir(image_folder):
                 if r.names[cls] == "person":
                     person_count += 1
 
-        # Update distribution
+        
         if person_count not in people_distribution:
             people_distribution[person_count] = 0
 
         people_distribution[person_count] += 1
 
-
-# Convert to dataframe
 distribution_df = pd.DataFrame(
     sorted(people_distribution.items()),
     columns=["people_in_image", "number_of_images"]
 )
 
-# Save CSV
 distribution_df.to_csv("person_distribution.csv", index=False)
 
 print("CSV saved as person_distribution.csv")
